84e970e9475853afa79bf4a1e77a7e2e9a3cb584
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2023-04-11 17:38:31 +0200

    add f=2

---
+++ first.py
@@ -0,0 +1,19 @@
+# import os
+#
+#
+# print(os.environ.get('DB_NAME'))
+# #
+# # while True:
+# #     pass
+#
+# for x in range(100):
+#     print(x)
+#
+# name = input('Enter your name: ')
+# age = input('Enter your age: ')
+#
+# print(f'Your name {name}. Your age {age}')
+
+print('Hello program started')
+f = 1
+f = 2
